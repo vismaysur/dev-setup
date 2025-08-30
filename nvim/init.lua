6b0835bffdfb622cb9c34922342b98755f759e3e
Vismay-dev <vismaysuramwar@gmail.com>
2025-08-30 13:06:48 -0500

    add neovim config

---
+++ nvim/init.lua
@@ -0,0 +1,9 @@
+-- Tab space settings
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.softtabstop = 4
+vim.opt.expandtab = true
+
+-- Relative + current line numbers
+vim.wo.relativenumber = true
+vim.opt.number = true
